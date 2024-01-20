5e862e11a5905dff27fdb340f122a6b46a45195c
tgondil <tgondil@purdue.edu>
2024-01-19 23:48:19 -0500

    More frontend work

--- frontend/src/components/chat/chat.tsx
+++ frontend/src/components/chat/chat.tsx
@@ -6,23 +6,26 @@ import { Slider } from '@mui/material';
 
 const Chat = () => {
   return (
-    <Grid className="body" container spacing={2} style={{backgroundColor: "#0B0D0E", height: "100vh"}}>
-    <Grid item xs={4}> </Grid>
-    <Grid item xs={8} style={{backgroundColor: "#0F1B29", }}>
-    </Grid>
-    </Grid>
-
-
-        <TextField id="filled-basic" label="Filled" variant="filled" sx={{
+    <Grid className="body" container spacing={2}>
+    <Grid item xs={1}> </Grid>
+    <Grid item xs={5}> 
+    <TextField id="filled-basic" label="Filled" variant="filled" sx={{
         input: {
           color: "white",
           background: "#0B0D0E",
-          width: "100rem",
+          width: "100%",
           paddingLeft: "0px",
           marginLeft: "0px",
           verticalAlign: "bottom",
         }
       }}/>
+    </Grid>
+    <Grid item xs={7} style={{backgroundColor: "#0F1B29", }}>
+    </Grid>
+
+
+        
+      </Grid>
   );
 };
 
