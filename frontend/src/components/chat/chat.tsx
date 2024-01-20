9b7c25efeed9b4c4a55abe45ab1c8245c3efebc0
Maxim Lavrenko <eternalbirch@gmail.com>
2024-01-19 23:47:45 -0500

    finishing up the slider

--- frontend/src/components/chat/chat.tsx
+++ frontend/src/components/chat/chat.tsx
@@ -1,28 +1,22 @@
-import React from 'react';
+import React from "react";
 import "./chat.css";
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import { Slider } from '@mui/material';
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import { Slider } from "@mui/material";
 
 const Chat = () => {
   return (
-    <Grid className="body" container spacing={2} style={{backgroundColor: "#0B0D0E", height: "100vh"}}>
-    <Grid item xs={4}> </Grid>
-    <Grid item xs={8} style={{backgroundColor: "#0F1B29", }}>
+    <Grid
+      className="body"
+      container
+      spacing={2}
+      style={{ backgroundColor: "#0B0D0E", height: "100vh" }}
+    >
+      <Grid item xs={4}>
+        {" "}
+      </Grid>
+      <Grid item xs={8} style={{ backgroundColor: "#0F1B29" }}></Grid>
     </Grid>
-    </Grid>
-
-
-        <TextField id="filled-basic" label="Filled" variant="filled" sx={{
-        input: {
-          color: "white",
-          background: "#0B0D0E",
-          width: "100rem",
-          paddingLeft: "0px",
-          marginLeft: "0px",
-          verticalAlign: "bottom",
-        }
-      }}/>
   );
 };
 
